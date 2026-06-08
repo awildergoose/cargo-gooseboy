c4dcc4e288420113b4a72b57386be584ee0f8fb4
awildergoose <61917452+awildergoose@users.noreply.github.com>
2026-06-08 22:53:12 +0300

    feat: making libraries

--- src/main.rs
+++ src/main.rs
@@ -8,16 +8,36 @@ use crate::commands::{build::run_build_command, new::run_new_command, pack::run_
 pub mod commands;
 pub mod utils;
 
+pub const CLAP_STYLING: clap::builder::styling::Styles = clap::builder::styling::Styles::styled()
+    .header(clap_cargo::style::HEADER)
+    .usage(clap_cargo::style::USAGE)
+    .literal(clap_cargo::style::LITERAL)
+    .placeholder(clap_cargo::style::PLACEHOLDER)
+    .error(clap_cargo::style::ERROR)
+    .valid(clap_cargo::style::VALID)
+    .invalid(clap_cargo::style::INVALID);
+
 #[derive(Parser)]
-#[command(name = "gooseboy")]
+#[command(name = "cargo")]
+#[command(bin_name = "cargo")]
+#[command(styles = CLAP_STYLING)]
 #[command(version = "1.0")]
 #[command(about = "gooseboy command line tool", long_about = None)]
 pub struct Cli {
     #[command(subcommand)]
-    command: Commands,
+    command: SCommands,
 }
 
 #[derive(Subcommand)]
+pub enum SCommands {
+    Gooseboy {
+        #[command(subcommand)]
+        command: Commands,
+    },
+}
+
+#[derive(Subcommand)]
+#[command(about = "gooseboy command line tool", long_about = None)]
 pub enum Commands {
     New {
         package: Option<String>,
@@ -25,6 +45,8 @@ pub enum Commands {
         buildscript: bool,
         #[arg(long)]
         no_std: bool,
+        #[arg(long, alias = "lib")]
+        library: bool,
     },
     Build {
         #[arg(short, long)]
@@ -44,14 +66,15 @@ pub enum Commands {
 pub fn main() -> Result<()> {
     env_logger::Builder::from_env(env_logger::Env::default().default_filter_or("info")).init();
 
-    let cli = Cli::parse();
+    let SCommands::Gooseboy { command } = Cli::parse().command;
 
-    match cli.command {
+    match command {
         Commands::New {
             package,
             no_std,
             buildscript,
-        } => run_new_command(package, no_std, buildscript)?,
+            library,
+        } => run_new_command(package, no_std, buildscript, !library)?,
         Commands::Build { release, package } => run_build_command(release, package)?,
         Commands::Pack {
             release,
